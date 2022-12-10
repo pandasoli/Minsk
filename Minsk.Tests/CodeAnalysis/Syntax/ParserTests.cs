6993ff3b00ebd25097f1a357eed8df5533514c40
pandasoli <sadpandasoli@gmail.com>
2022-12-10 04:48:11 -0300

    Add tests to ensure we honor unary and binary precedences

--- Minsk.Tests/CodeAnalysis/Syntax/ParserTests.cs
+++ Minsk.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -71,10 +71,70 @@ namespace Minsk.Tests.CodeAnalysis.Syntax
       }
     }
 
+    [Theory]
+    [MemberData(nameof(GetUnaryOpPairsData))]
+    public void Parser_UnaryExpr_HonorsPreces(SyntaxKind unaryKind, SyntaxKind binKind) {
+      var unaryPrece = SyntaxFacts.GetUnaryOpPrece(unaryKind);
+      var unaryText = SyntaxFacts.GetText(unaryKind);
+
+      var binPrece = SyntaxFacts.GetBinOpPrece(binKind);
+      var binText = SyntaxFacts.GetText(binKind);
+
+      var text = $"{unaryText} a {binText} b";
+      var expr = SyntaxTree.Parse(text).Root;
+
+      if (unaryPrece >= binPrece) {
+        /*
+              bin
+             /  \
+          unary  b
+           |
+           a
+        */
+
+        using (var e = new AssertingEnumerator(expr)) {
+          e.AssertNode(SyntaxKind.BinExpr);
+          e.AssertNode(SyntaxKind.UnaryExpr);
+          e.AssertToken(unaryKind, unaryText);
+          e.AssertNode(SyntaxKind.NameExpr);
+          e.AssertToken(SyntaxKind.IdTk, "a");
+          e.AssertToken(binKind, binText);
+          e.AssertNode(SyntaxKind.NameExpr);
+          e.AssertToken(SyntaxKind.IdTk, "b");
+        }
+      }
+      else {
+        /*
+           unary
+            |
+           bin
+           / \
+          a   b
+        */
+
+        using (var e = new AssertingEnumerator(expr)) {
+          e.AssertNode(SyntaxKind.UnaryExpr);
+          e.AssertToken(unaryKind, unaryText);
+          e.AssertNode(SyntaxKind.BinExpr);
+          e.AssertNode(SyntaxKind.NameExpr);
+          e.AssertToken(SyntaxKind.IdTk, "a");
+          e.AssertToken(binKind, binText);
+          e.AssertNode(SyntaxKind.NameExpr);
+          e.AssertToken(SyntaxKind.IdTk, "b");
+        }
+      }
+    }
+
     private static IEnumerable<object[]> GetBinOpPairsData() {
       foreach (var op1 in SyntaxFacts.GetBinOpKinds())
         foreach (var op2 in SyntaxFacts.GetBinOpKinds())
           yield return new object[] { op1, op2 };
+    }
+
+    private static IEnumerable<object[]> GetUnaryOpPairsData() {
+      foreach (var unary in SyntaxFacts.GetUnaryOpKinds())
+        foreach (var bin in SyntaxFacts.GetBinOpKinds())
+          yield return new object[] { unary, bin };
     }
   }
 }
