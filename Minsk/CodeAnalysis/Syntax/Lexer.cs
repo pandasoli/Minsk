6f74d84356f234328b5a6f54ec4f08fa06d33036
pandasoli <sadpandasoli@gmail.com>
2023-01-08 23:33:23 -0300

    cleaned up

--- Minsk/CodeAnalysis/Syntax/Lexer.cs
+++ Minsk/CodeAnalysis/Syntax/Lexer.cs
@@ -3,9 +3,14 @@ namespace Minsk.CodeAnalysis.Syntax
 {
   internal sealed class Lexer
   {
-    private DiagBag _diags = new DiagBag();
+    private readonly DiagBag _diags = new DiagBag();
     private readonly string _text;
+
     private int _pos;
+
+    private int _start;
+    private SyntaxKind _kind;
+    private object? _val;
 
     public Lexer(string text) {
       _text = text;
@@ -26,108 +31,116 @@ namespace Minsk.CodeAnalysis.Syntax
       return _text[idx];
     }
 
-    private void Next() {
-      _pos++;
-    }
-
 
     public SyntaxToken Lex()
     {
-      if (_pos >= _text.Length)
-        return new SyntaxToken(SyntaxKind.EOFTk, _pos, "\0", null);
+      _start = _pos;
+      _kind = SyntaxKind.BadTk;
+      _val = null;
 
-      var start = _pos;
+      switch (Current) {
+        case '\0':         _kind = SyntaxKind.EOFTk;        break;
+        case '+':  _pos++; _kind = SyntaxKind.PlusTk;       break;
+        case '-':  _pos++; _kind = SyntaxKind.DashTk;       break;
+        case '*':  _pos++; _kind = SyntaxKind.StarTk;       break;
+        case '/':  _pos++; _kind = SyntaxKind.SlashTk;      break;
+        case '(':  _pos++; _kind = SyntaxKind.OpenParenTk;  break;
+        case ')':  _pos++; _kind = SyntaxKind.CloseParenTk; break;
 
-      if (char.IsDigit(Current))
-      {
-        while (char.IsDigit(Current))
-          Next();
+        case '&':
+          if (Lookahead == '&') {
+            _kind = SyntaxKind.ApsdApsdTk;
+            _pos += 2;
+          }
 
-        var len = _pos - start;
-        var buff = _text.Substring(start, len);
-        if (!int.TryParse(buff, out var res))
-          _diags.ReportInvNum(new TextSpan(start, len), buff, typeof(int));
+          break;
 
-        return new SyntaxToken(SyntaxKind.NumberTk, start, buff, res);
+        case '|':
+          if (Lookahead == '|') {
+            _kind = SyntaxKind.PipePipeTk;
+            _pos += 2;
+          }
+
+          break;
+
+        case '=':
+          _pos++;
+
+          if (Current == '=') {
+            _kind = SyntaxKind.EqsEqsTk;
+            _pos++;
+          }
+          else
+            _kind = SyntaxKind.EqsTk;
+
+          break;
+
+        case '!':
+          _pos++;
+
+          if (Current == '=') {
+            _kind = SyntaxKind.BangEqsTk;
+            _pos++;
+          }
+          else
+            _kind = SyntaxKind.BangTk;
+
+          break;
+
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+          ReadNumTk();
+          break;
+
+        case ' ': case '\t': case '\n': case '\r':
+          ReadWhiteSpaceTk();
+          break;
+
+        default:
+          if      (char.IsLetter(Current))     ReadIdOrKw();
+          // The up code only cover the most used whites paces
+          else if (char.IsWhiteSpace(Current)) ReadWhiteSpaceTk();
+          else {
+            _diags.ReportBadCh(_pos, Current);
+            _pos++;
+          }
+
+          break;
       }
-      else if (char.IsWhiteSpace(Current))
-      {
-        while (char.IsWhiteSpace(Current))
-          Next();
 
-        var len = _pos - start;
-        var buff = _text.Substring(start, len);
+      int len = _pos - _start;
+      string buff = SyntaxFacts.GetText(_kind) ?? _text.Substring(_start, len);
 
-        return new SyntaxToken(SyntaxKind.WhiteSpaceTk, start, buff, null);
-      }
-      else if (char.IsLetter(Current))
-      {
-        while (char.IsLetter(Current))
-          Next();
+      return new SyntaxToken(_kind, _start, buff, _val);
+    }
 
-        var len = _pos - start;
-        var buff = _text.Substring(start, len);
-        var kind = SyntaxFacts.GetKeywordKind(buff);
+    private void ReadNumTk() {
+      while (char.IsDigit(Current))
+        _pos++;
 
-        return new SyntaxToken(kind, start, buff, null);
-      }
-      else
-      {
-        var kind = SyntaxKind.BadTk;
-        var pos = _pos + 1;
-        var buff = _text[_pos].ToString();
+      var len = _pos - _start;
+      var buff = _text.Substring(_start, len);
+      if (!int.TryParse(buff, out var res))
+        _diags.ReportInvNum(new TextSpan(_start, len), _text, typeof(int));
 
-        switch (Current) {
-          case '+': kind = SyntaxKind.PlusTk;       break;
-          case '-': kind = SyntaxKind.DashTk;       break;
-          case '*': kind = SyntaxKind.StarTk;       break;
-          case '/': kind = SyntaxKind.SlashTk;      break;
-          case '(': kind = SyntaxKind.OpenParenTk;  break;
-          case ')': kind = SyntaxKind.CloseParenTk; break;
-          case '&':
-            if (Lookahead == '&') {
-              kind = SyntaxKind.ApsdApsdTk;
-              pos++;
-              buff = "&&";
-            }
+      _val = res;
+      _kind = SyntaxKind.NumberTk;
+    }
 
-            break;
-          case '|':
-            if (Lookahead == '|') {
-              kind = SyntaxKind.PipePipeTk;
-              pos++;
-              buff = "||";
-            }
+    private void ReadWhiteSpaceTk() {
+      while (char.IsWhiteSpace(Current))
+        _pos++;
 
-            break;
-          case '=':
-            if (Lookahead == '=') {
-              kind = SyntaxKind.EqsEqsTk;
-              pos++;
-              buff = "==";
-            }
-            else
-              kind = SyntaxKind.EqsTk;
+      _kind = SyntaxKind.WhiteSpaceTk;
+    }
 
-            break;
-          case '!':
-            if (Lookahead == '=') {
-              kind = SyntaxKind.BangEqsTk;
-              pos++;
-              buff = "!=";
-            }
-            else
-              kind = SyntaxKind.BangTk;
+    private void ReadIdOrKw() {
+      while (char.IsLetter(Current))
+        _pos++;
 
-            break;
-        }
-
-        if (kind == SyntaxKind.BadTk)
-          _diags.ReportBadCh(pos, Current);
-
-        _pos = pos;
-        return new SyntaxToken(kind, start, buff, null);
-      }
+      var len = _pos - _start;
+      var buff = _text.Substring(_start, len);
+      _kind = SyntaxFacts.GetKeywordKind(buff);
     }
   }
 
