7e8b5789ec96a9169a79fc829abe0bbae5540925
pandasoli <sadpandasoli@gmail.com>
2023-01-10 18:05:46 -0300

    Replace is-checks with switch in Eval

--- Minsk/CodeAnalysis/Eval.cs
+++ Minsk/CodeAnalysis/Eval.cs
@@ -17,50 +17,62 @@ namespace Minsk.CodeAnalysis
     }
 
     private object? EvalExpr(BoundExpr node) {
-      if (node is BoundLitExpr num)
-        return num.Val;
+      switch (node.Kind)
+      {
+        case BoundNodeKind.LitExpr:      return EvalLitExpr((BoundLitExpr)          node);
+        case BoundNodeKind.VarExpr:      return EvalVarExpr((BoundVarExpr)          node);
+        case BoundNodeKind.AssignmsExpr: return EvalAssignmExpr((BoundAssignmsExpr) node);
+        case BoundNodeKind.UnaryExpr:    return EvalUnaryExpr((BoundUnaryExpr)      node);
+        case BoundNodeKind.BinaryExpr:      return EvalBinExpr((BoundBinExpr)       node);
 
-      if (node is BoundVarExpr var)
-        return _vars[var.Var];
-
-      if (node is BoundAssignmsExpr ass) {
-        var val = EvalExpr(ass.Expr);
-        _vars[ass.Var] = val;
-        return val;
+        default:
+          throw new Exception($"Unexpected node {node.Kind}.");
       }
+    }
 
-      if (node is BoundUnaryExpr unary) {
-        var operand = EvalExpr(unary.Operand);
+    private object EvalLitExpr(BoundLitExpr num) {
+      return num.Val;
+    }
 
-        if (unary.Op.Kind == BoundUnaryOpKind.Identity) return  (int)  (operand ?? 0);
-        if (unary.Op.Kind == BoundUnaryOpKind.Neg)      return -(int)  (operand ?? 0);
-        if (unary.Op.Kind == BoundUnaryOpKind.LgcNeg)   return !(bool) (operand ?? true);
+    private object? EvalVarExpr(BoundVarExpr var) {
+      return _vars[var.Var];
+    }
 
-        throw new Exception($"Unexpected unary operator {unary.Op}.");
+    private object? EvalAssignmExpr(BoundAssignmsExpr ass) {
+      var val = EvalExpr(ass.Expr);
+      _vars[ass.Var] = val;
+      return val;
+    }
+
+    private object EvalUnaryExpr(BoundUnaryExpr unary) {
+      var operand = EvalExpr(unary.Operand);
+
+      if (unary.Op.Kind == BoundUnaryOpKind.Identity) return  (int)  (operand ?? 0);
+      if (unary.Op.Kind == BoundUnaryOpKind.Neg)      return -(int)  (operand ?? 0);
+      if (unary.Op.Kind == BoundUnaryOpKind.LgcNeg)   return !(bool) (operand ?? true);
+
+      throw new Exception($"Unexpected unary operator {unary.Op}.");
+    }
+
+    private object EvalBinExpr(BoundBinExpr bin) {
+      var left = EvalExpr(bin.Left);
+      var right = EvalExpr(bin.Right);
+
+      switch (bin.Op.Kind) {
+        case BoundBinOpKind.Add: return (int) (left ?? 0) + (int) (right ?? 0);
+        case BoundBinOpKind.Sub: return (int) (left ?? 0) - (int) (right ?? 0);
+        case BoundBinOpKind.Mul: return (int) (left ?? 0) * (int) (right ?? 0);
+        case BoundBinOpKind.Div: return (int) (left ?? 0) / (int) (right ?? 0);
+
+        case BoundBinOpKind.LgcAnd: return (bool) (left ?? false) && (bool) (right ?? false);
+        case BoundBinOpKind.LgcOr:  return (bool) (left ?? false) || (bool) (right ?? false);
+
+        case BoundBinOpKind.Eqs:    return  Equals(left, right);
+        case BoundBinOpKind.NotEqs: return !Equals(left, right);
+
+        default:
+          throw new Exception($"Unexpected binary operator {bin.Op}.");
       }
-
-      if (node is BoundBinExpr bin) {
-        var left = EvalExpr(bin.Left);
-        var right = EvalExpr(bin.Right);
-
-        switch (bin.Op.Kind) {
-          case BoundBinOpKind.Add: return (int) (left ?? 0) + (int) (right ?? 0);
-          case BoundBinOpKind.Sub: return (int) (left ?? 0) - (int) (right ?? 0);
-          case BoundBinOpKind.Mul: return (int) (left ?? 0) * (int) (right ?? 0);
-          case BoundBinOpKind.Div: return (int) (left ?? 0) / (int) (right ?? 0);
-
-          case BoundBinOpKind.LgcAnd: return (bool) (left ?? false) && (bool) (right ?? false);
-          case BoundBinOpKind.LgcOr:  return (bool) (left ?? false) || (bool) (right ?? false);
-
-          case BoundBinOpKind.Eqs:    return  Equals(left, right);
-          case BoundBinOpKind.NotEqs: return !Equals(left, right);
-
-          default:
-            throw new Exception($"Unexpected binary operator {bin.Op}.");
-        }
-      }
-
-      throw new Exception($"Unexpected node {node.Kind}.");
     }
   }
 }
